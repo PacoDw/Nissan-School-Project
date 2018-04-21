11714bf2792a4fb87324e4e83c40f5bdde60931e
Paco Preciado Mendoza <__pm@outlook.com>
2018-04-20 19:43:40 -0500

    :sparkles: Add dotenv, express-session and bcrypt config

--- app.js
+++ app.js
@@ -1,64 +1,78 @@
-const createError  = require('http-errors');
 const express      = require('express');
+const createError  = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger       = require('morgan');
 const bodyParser   = require('body-parser');
 
+// Set the enviroment variables-----------------------------------------------------
+require('dotenv').config();
+
+
 const app = express();
 
-// Global variables that help us to handle body parse------
-const urlencodedParse = bodyParser.urlencoded({extended: false});
-const jsonParse       = bodyParser.json();
+// Authentication packages----------------------------------------------------------
+const bcrypt       = require('bcrypt');
+const session      = require('express-session');
 
+// Routes imports-------------------------------------------------------------------
 const indexRouter  = require('./routes/index');
-const teamRouter  = require('./routes/team');
+const register     = require('./routes/register');
+const login        = require('./routes/login');
+const teamRouter   = require('./routes/team');
 
 
-// view engine setup---------------------------------------
+// view engine setup----------------------------------------------------------------
 app
     .set('views', `${__dirname}/views`)
     .set('view engine', 'hbs');
 
 
-// Middlewares----------------------------------------------
+// Middlewares----------------------------------------------------------------------
 app
     .use(logger('dev'))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
     .use(cookieParser())
-  //Setting folder public like assets to the client side--
-    .use('assets', express.static(`${__dirname}/public`));
+    .use('/assets', express.static(`${__dirname}/public`)) //Setting folder public like assets to the client side
+
+    .use(session({
+      secret: 'iovjcxzoivjewqn',
+      resave: false,
+      saveUninitialized: false  // Initiating the session without you have logged 
+    }))
     
 
-//-------------------------------------------------------
-// Handling Routes---------------------------------------
+//----------------------------------------------------------------------------------
+// Handling Routes------------------------------------------------------------------
 app
     .use('/', indexRouter)
+    .use('/register', register)
+    .use('/login', login)
     .use('/team', teamRouter);
 
-
-
-//--------------------------------------------------------
-// catch 404 and forward to error handler-----------------
+//-----------------------------------------------------------------------------------
+// catch 404 and forward to error handler--------------------------------------------
 app    
     .use(function(req, res, next) {
       next(createError(404));
     })
     
-  // error handler-----------------------------------------
+  // error handler-------------------------------------------------------------------
     .use(function(err, req, res, next) {
       // set locals, only providing error in development
       res.locals.message = err.message;
       res.locals.error = req.app.get('env') === 'development' ? err : {};
     
-  // render the error page---------------------------------
+  // render the error page-----------------------------------------------------------
       res.status(err.status || 500);
       res.render('error');
     });
 
-// Handlebars default config-------------------------------
+// Handlebars default config---------------------------------------------------------
 const hbs = require('hbs');
 const fs = require('fs');
 
-const partialsDir = __dirname + '/views';
+const partialsDir = __dirname + '/views/partials';
 
 const filenames = fs.readdirSync(partialsDir);
 
