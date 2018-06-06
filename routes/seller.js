c4ba75235845191a670ee27f3d17679c9a7dc264
Paco Preciado Mendoza <__pm@outlook.com>
2018-06-06 09:52:59 -0500

    :sparkles: Add new route to add and delete a user

--- routes/seller.js
+++ routes/seller.js
@@ -1,5 +1,5 @@
 const express  = require('express');
-const auth	   = require('../authUsers/authUser'); 
+const auth	   = require('../authAccounts/authAccount'); 
 const fs = require('fs');
 
 const router   = express.Router();
@@ -10,15 +10,6 @@ router
 		console.log("Seller--------------------------");
 		// console.log(req.user);
 		// console.log(req.isAuthenticated());
-
-
-		fs.readdir(`./views`, (err, files) => {
-			console.log('Views--------------------------------')
-		files.forEach(file => {
-			console.log(file);
-		});
-			console.log('-------------------------------------')
-		})
 
 		const db = require('../db/my_database');
 
@@ -31,9 +22,6 @@ router
 			console.log('-----------------------------')
 			let r = JSON.stringify(results, undefined, 2);
 			// res.json(results)
-
-		
-		
 
 			res.render('Seller', {
 				titlePage     : 'Seller', 
@@ -60,66 +48,58 @@ router
 	// ----------------------------------------------------------------------------------------
 	// Create New Seller - We need add the restrict middleware auth.justManagers(),
 	.post('/addSeller',  (req, res) => {
-		console.log('-----------------------------')
-		console.log('Post New Seller');
-		console.log(req.body);
-		console.log('-----------------------------')
-
-		res.status(500).send({ messageFlash : 'Error con mysql' });
 
 		// Conection to the database
 		const db = require('../db/my_database');
 
 			// Inserting a new user withing the database
 			db.users.query("CALL newSeller(?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-			[req.body.name, req.body.lastname, req.body.phone, req.body.address, req.body.city, 
-			req.body.state, req.body.postal_code, req.body.country, req.body.selected.id_office_manager], 
-
+				[req.body.name, req.body.lastname, req.body.phone, req.body.address, req.body.city, 
+				req.body.state, req.body.postal_code, req.body.country, req.body.selected.id], 
 			(err, results, fields) => {
-				if (err){
-
-					console.log('Entro errror');
-
-					res.status(200).send({ messageFlash : err });
-					res.end();
-					// throw err;
-				} 
-					
-
-				console.log(results);
-				if (results[0][0].error)
-				{
-					console.log('Error normal----------------------------')
-					console.log(results[0][0])
-					console.log('-----------------------------');
-					// req.flash('info', { messageFlash: results[0][0].error } );
-					// res.redirect('/Seller/NewSeller');
-
-					res.send(200, { messageFlash : results[0][0].error });
+				if (err)  {
+					console.log('Error MysSQL: ', err);
+					res.status(400).send({ messageFlash : 'Error Interno: Vuelva intentarlo mas tarde.' }); 
 				}
+				else if (results[0][0].error)
+					res.status(400).send({ messageFlash : results[0][0].error });
 				else
-				{
-					console.log('True-----------------------------')
-					// console.log(results[0][0])
-					console.log('-----------------------------')
-	
-					// req.flash('info', { messageFlash: results[0][0].success } );
-	
-					// res.redirect('/Seller');
-					res.send(200, { messageFlash : results[0][0].success });
+					res.status(200).send({ messageFlash : results[0][0].success });
+			}
+		);                    
+	})
 
+	// ----------------------------------------------------------------------------------------
+	// Create New Seller - We need add the restrict middleware auth.justManagers(),
+	.delete('/deleteSeller/:id',  (req, res) => {
+
+		console.log('Paramas: ', req.params.id);
+
+		// Conection to the database
+		const db = require('../db/my_database');
+
+			// Inserting a new user withing the database
+			db.users.query("Call deleteSeller(?)", [req.params.id], 
+			(err, results, fields) => {
+				if (err)  {
+					console.log('Error MysSQL: ', err);
+					res.status(400).send({ messageFlash : 'Error Interno: Vuelva intentarlo mas tarde.' }); 
 				}
+				else if (results[0][0].error)
+					res.status(400).send({ messageFlash : results[0][0].error });
+				else
+					res.status(200).send({ messageFlash : results[0][0].success });
 			}
 		);                    
 	})
 
 
 	// Get a JSON DATA OF THE ALL SELLER WITHIN THE DATABASE
-	.get('/Sellers.json', (req, res) => {
+	.get('/SellersWithoutAccount', (req, res) => {
 
 		const db = require('../db/my_database');
 
-		db.users.query("SELECT s.id_seller, s.name FROM sellers as s", 
+		db.users.query("SELECT s.id_seller, s.name FROM sellers as s WHERE id_account = 1", 
 			[], 
 
 			(err, results, fields) => {
@@ -130,9 +110,23 @@ router
 
 			res.json(results);
 		});
-
-			// res.send(JSON.stringify(resusts))
 	})
+
+	.get('/allSellers', (req, res) => {
+		const db = require('../db/my_database');
+
+		db.users.query("SELECT * FROM sellers as s", 
+			[], 
+			(err, results, fields) => {
+			
+			console.log('-----------------------------')
+			console.log(results);
+			console.log('-----------------------------')
+
+			res.json(results);
+		});
+		})
+
 
 
 module.exports = router;
