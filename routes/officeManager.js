256e0218f4a779299cc235ea81695f3c978c43d8
Paco Preciado Mendoza <__pm@outlook.com>
2018-06-09 18:02:40 -0500

    :building_construction: Refactoring the code

--- routes/officeManager.js
+++ routes/officeManager.js
@@ -6,16 +6,37 @@ const router  = express.Router();
 // Office Manager Route---------------------------------------------------------------------
 router
 
-// ------------------------------------------------------------------------------------
-    // We need add the restrict middleware
     .get('/', (req, res) => {
-        console.log('-----------------------------')
-        console.log('Route global manager')
+        res.redirect('/sellers');
+    })
 
-        res.render('TempApp', {
+    // ------------------------------------------------------------------------------------
+    // We need add the restrict middleware
+    .get('/sellers', (req, res) => {
+        console.log('-----------------------------')
+        console.log('Route office manager')
+
+        res.render('OfficeManagerApp', {
             user      : 'Paco Preciado',//req.user,
             auth      : true, //req.isAuthenticated(),
             typeUser  : 'officeManager',
+            page      : 'sellers',
+            titlePage : 'Testing Nissan',
+            messageFlash: req.flash('info'),
+        });
+    })
+
+    // ------------------------------------------------------------------------------------
+    // We need add the restrict middleware
+    .get('/accounts', (req, res) => {
+        console.log('-----------------------------')
+        console.log('Route office manager')
+
+        res.render('OfficeManagerApp', {
+            user      : 'Paco Preciado',//req.user,
+            auth      : true, //req.isAuthenticated(),
+            typeUser  : 'officeManager',
+            page      : 'accounts',
             titlePage : 'Testing Nissan',
             messageFlash: req.flash('info'),
         });
@@ -25,11 +46,11 @@ router
     // We need add the restrict middleware
     .get('/allOfficeManagers', (req, res) => {
         console.log('-----------------------------')
-        console.log('Route All global manager Users')
+        console.log('Route All office manager Users')
 
         const db = require('../db/my_database');
 
-        db.users.query("SELECT gm.id_global_manager, gm.name FROM globals_managers as gm",
+        db.users.query("SELECT gm.id_global_manager, gm.name, gm.job FROM globals_managers as gm",
             [],
             (err, results, fields) => {
                 console.log('ID OFFICE: ', results);
@@ -39,37 +60,6 @@ router
                 }
                 else
                     res.status(200).send( results );
-            }
-        );
-    })
-
-
-
-    // ------------------------------------------------------------------------------------
-    // Returns all the sellers 
-    .get('/sellers', auth.justManagers(), (req, res) => {
-        console.log('-----------------------------')
-        console.log('Route global manager Sellers')
-
-        const db = require('../db/my_database');
-
-        db.users.query("SELECT s.id_seller, s.name FROM sellers as s",
-            [],
-            (err, results, fields) => {
-
-                // console.log('-----------------------------')
-                // console.log(JSON.stringify(results, undefined, 2));
-                // console.log('-----------------------------')
-                let r = JSON.stringify(results, undefined, 2);
-                // res.json(results);
-                res.render('Test', {
-                    user      : 'Paco Preciado',//req.user,
-                    auth      : true, //req.isAuthenticated(),
-                    typeUser  : 'officeManager',
-                    titlePage : 'Testing Nissan',
-                    messageFlash: req.flash('info'),
-                    options: r,
-                });
             }
         );
     })
