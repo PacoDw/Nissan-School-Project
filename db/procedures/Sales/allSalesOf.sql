cea71b0dac84862f6314e96eb66062ec1486d1d9
Paco Preciado Mendoza <__pm@outlook.com>
2018-06-13 11:39:09 -0500

    :sparkles: Add show all sales of an user like procedure

---
+++ db/procedures/Sales/allSalesOf.sql
@@ -0,0 +1,25 @@
+DROP PROCEDURE IF EXISTS allSalesOf;
+DELIMITER //
+CREATE PROCEDURE allSalesOf (_id_seller INTEGER)
+
+DETERMINISTIC
+BEGIN
+    SELECT s.id_sale, car.name as car_name, cus.name as customer_name,  st.status_sale, s.deposit, cm.cost
+    FROM sales as s
+    INNER JOIN cars as car ON car.id_car = s.id_car
+    INNER JOIN cars_models as cm ON cm.id_car_model = car.id_car_model
+    INNER JOIN customers as cus ON cus.id_customer = s.id_customer
+    INNER JOIN status_sales as st ON st.id_status_sale = s.id_status_sale
+    WHERE s.id_seller = _id_seller;
+END
+// DELIMITER ;
+
+-- TESTING ------------------------------------------------------------------------------
+
+SELECT * FROM sales;
+CALL allSalesOf(1);
+
+SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS=1;
+
+use nissan_db;
