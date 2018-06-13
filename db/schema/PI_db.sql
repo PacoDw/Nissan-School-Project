03da975ca928d30305a8e97308fd2b31d4adc473
Paco Preciado Mendoza <__pm@outlook.com>
2018-06-13 11:35:40 -0500

    :boom: Changing the logic of the schema

--- db/schema/PI_db.sql
+++ db/schema/PI_db.sql
@@ -91,102 +91,102 @@ VALUES ('Sucursal Colima', '3123450956', 'Centro calle nose #8643', 'Colima','Co
 INSERT INTO offices (name_office, phone, address, city, state, postal_code, country, id_global_manager, id_office_manager)
 VALUES ('Sucursal Manzanillo', '3143478056', 'Centro  #3773', 'Manzanillo','Colima', '28077', 'Mexico', 1, 2);
 
--- VEHICLES STATUS----------------------------------------------------------------------------------------------
-CREATE TABLE vehicles_status (
-    id_vehicle_status    INTEGER AUTO_INCREMENT NOT NULL,
+-- carS STATUS----------------------------------------------------------------------------------------------
+CREATE TABLE cars_status (
+    id_car_status    INTEGER AUTO_INCREMENT NOT NULL,
     status               VARCHAR(35) NOT NULL,
 
-    PRIMARY KEY (id_vehicle_status)
+    PRIMARY KEY (id_car_status)
 );
-INSERT INTO vehicles_status (status) VALUES ('Disponible');
-INSERT INTO vehicles_status (status) VALUES ('Vendido');
-INSERT INTO vehicles_status (status) VALUES ('Apartado');
-INSERT INTO vehicles_status (status) VALUES ('Agotado');
+INSERT INTO cars_status (status) VALUES ('Disponible');
+INSERT INTO cars_status (status) VALUES ('Vendido');
+INSERT INTO cars_status (status) VALUES ('Apartado');
+INSERT INTO cars_status (status) VALUES ('Agotado');
 
--- VEHICLE MODELS
-CREATE TABLE vehicles_models (
-    id_vehicle_model    INTEGER AUTO_INCREMENT NOT NULL,
+-- car MODELS
+CREATE TABLE cars_models (
+    id_car_model    INTEGER AUTO_INCREMENT NOT NULL,
     model               VARCHAR(100) NOT NULL,
     details             VARCHAR(550) NOT NULL,
     cost                DOUBLE NOT NULL,
 
-    PRIMARY KEY (id_vehicle_model)
+    PRIMARY KEY (id_car_model)
 );
 -- MODELOS KIKS  -> We can create one table for each model
-INSERT INTO vehicles_models (model, details, cost) VALUES ('KIKS EDICION ESPECIAL DARK LIGHT', 'Sin descripción', 374800);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('KIKS SENSE 5MT', 'Sin descripción', 298000);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('KIKS ADVANCE CVT', 'Sin descripción', 333300);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('KIKS EXCLUSIVE CVT', 'Sin descripción', 371800);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('KIKS BITONO', 'Sin descripción', 373000);
+INSERT INTO cars_models (model, details, cost) VALUES ('KIKS EDICION ESPECIAL DARK LIGHT', 'Sin descripción', 374800);
+INSERT INTO cars_models (model, details, cost) VALUES ('KIKS SENSE 5MT', 'Sin descripción', 298000);
+INSERT INTO cars_models (model, details, cost) VALUES ('KIKS ADVANCE CVT', 'Sin descripción', 333300);
+INSERT INTO cars_models (model, details, cost) VALUES ('KIKS EXCLUSIVE CVT', 'Sin descripción', 371800);
+INSERT INTO cars_models (model, details, cost) VALUES ('KIKS BITONO', 'Sin descripción', 373000);
 -- MODELOS VERSA
-INSERT INTO vehicles_models (model, details, cost) VALUES ('VERSA DRIVE M/T 1.6L', 'Sin descripción', 183100);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('VERSA DRIVE A/T 1.6L', 'Sin descripción', 199000);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('VERSA SENSE M/T 1.6L', 'Sin descripción', 217900);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('VERSA SENSE A/T 1.6L', 'Sin descripción', 235300);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('VERSA ADVANCE M/T 1.6L', 'Sin descripción', 237300);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('VERSA ADVANCE A/T 1.6L', 'Sin descripción', 254900);
-INSERT INTO vehicles_models (model, details, cost) VALUES ('VERSA EXCLUSIVE A/T 1.6L', 'Sin descripción', 271300);
+INSERT INTO cars_models (model, details, cost) VALUES ('VERSA DRIVE M/T 1.6L', 'Sin descripción', 183100);
+INSERT INTO cars_models (model, details, cost) VALUES ('VERSA DRIVE A/T 1.6L', 'Sin descripción', 199000);
+INSERT INTO cars_models (model, details, cost) VALUES ('VERSA SENSE M/T 1.6L', 'Sin descripción', 217900);
+INSERT INTO cars_models (model, details, cost) VALUES ('VERSA SENSE A/T 1.6L', 'Sin descripción', 235300);
+INSERT INTO cars_models (model, details, cost) VALUES ('VERSA ADVANCE M/T 1.6L', 'Sin descripción', 237300);
+INSERT INTO cars_models (model, details, cost) VALUES ('VERSA ADVANCE A/T 1.6L', 'Sin descripción', 254900);
+INSERT INTO cars_models (model, details, cost) VALUES ('VERSA EXCLUSIVE A/T 1.6L', 'Sin descripción', 271300);
 
--- VEHICLES-------------------------------------------------------------------------------------------------------
-CREATE TABLE vehicles (
-    id_vehicle 		    INTEGER AUTO_INCREMENT NOT NULL,
+-- carS-------------------------------------------------------------------------------------------------------
+CREATE TABLE cars (
+    id_car 		    INTEGER AUTO_INCREMENT NOT NULL,
     name                VARCHAR(35) NOT NULL,
     details             VARCHAR(250) NOT NULL,
-    id_vehicle_model    INTEGER NOT NULL,
-    id_vehicle_status   INTEGER NOT NULL,
+    id_car_model    INTEGER NOT NULL,
+    id_car_status   INTEGER NOT NULL,
 
-    PRIMARY KEY (id_vehicle, id_vehicle_status, id_vehicle_model),
+    PRIMARY KEY (id_car, id_car_status, id_car_model),
 
-    FOREIGN KEY (id_vehicle_model)
-    REFERENCES vehicles_models (id_vehicle_model),
+    FOREIGN KEY (id_car_model)
+    REFERENCES cars_models (id_car_model),
 
-    FOREIGN KEY (id_vehicle_status)
-    REFERENCES vehicles_status (id_vehicle_status)
+    FOREIGN KEY (id_car_status)
+    REFERENCES cars_status (id_car_status)
 );
 -- KIKS
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('KIKS', 'Sin descripción', 1, 1);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('KIKS', 'Sin descripción', 2, 2);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('KIKS', 'Sin descripción', 3, 3);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('KIKS', 'Sin descripción', 4, 4);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('KIKS', 'Sin descripción', 5, 1);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('KIKS', 'Sin descripción', 1, 1);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('KIKS', 'Sin descripción', 2, 2);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('KIKS', 'Sin descripción', 3, 3);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('KIKS', 'Sin descripción', 4, 4);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('KIKS', 'Sin descripción', 5, 1);
 -- VERSAS
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('VERSA', 'Sin descripción', 6, 1);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('VERSA', 'Sin descripción', 7, 2);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('VERSA', 'Sin descripción', 8, 3);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('VERSA', 'Sin descripción', 9, 4);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('VERSA', 'Sin descripción', 10, 1);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('VERSA', 'Sin descripción', 11, 2);
-INSERT INTO vehicles(name, details, id_vehicle_model, id_vehicle_status) VALUES ('VERSA', 'Sin descripción', 12, 3);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('VERSA', 'Sin descripción', 6, 1);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('VERSA', 'Sin descripción', 7, 2);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('VERSA', 'Sin descripción', 8, 3);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('VERSA', 'Sin descripción', 9, 4);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('VERSA', 'Sin descripción', 10, 1);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('VERSA', 'Sin descripción', 11, 2);
+INSERT INTO cars(name, details, id_car_model, id_car_status) VALUES ('VERSA', 'Sin descripción', 12, 3);
 
 -- STOCKS
 CREATE TABLE stocks (
     id_stock        INTEGER AUTO_INCREMENT NOT NULL,
     data_at         DATETIME NOT NULL,
-    id_vehicle      INTEGER NOT NULL,
+    id_car      INTEGER NOT NULL,
     id_office       INTEGER NOT NULL,
 
-    PRIMARY KEY (id_stock, id_vehicle, id_office),
+    PRIMARY KEY (id_stock, id_car, id_office),
 
-    FOREIGN KEY (id_vehicle)
-    REFERENCES vehicles (id_vehicle),
+    FOREIGN KEY (id_car)
+    REFERENCES cars (id_car),
 
     FOREIGN KEY (id_office)
     REFERENCES offices (id_office)
 );
 -- KIKS are in Manzanillo office
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 1, 1);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 2, 1);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 3, 1);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 4, 1);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 5, 1);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 1, 1);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 2, 1);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 3, 1);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 4, 1);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 5, 1);
 -- VERSAS are in Colima office
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 6, 2);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 7, 2);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 8, 2);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 9, 2);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 10, 2);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 11, 2);
-INSERT INTO stocks (data_at, id_vehicle, id_office) VALUES ('2018-04-02 1:26:59', 12, 2);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 6, 2);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 7, 2);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 8, 2);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 9, 2);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 10, 2);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 11, 2);
+INSERT INTO stocks (data_at, id_car, id_office) VALUES ('2018-04-02 1:26:59', 12, 2);
 
 -- STOCKS MATRIX-----------------------------------------------------------------------------------------
 CREATE TABLE stock_matrix (
@@ -282,16 +282,16 @@ INSERT INTO customers (name, lastname, phone, address, city, state, postal_code,
 VALUES ('Paul', 'Betancur', '3141230077','calle nose #2113', 'Guadalajara','Jalisco', '3233', 'Mexico', 2);
 
 -- TYPES ORDERS------------------------------------------------------------------------------------------------------
-CREATE TABLE orders_types (
-    id_order_type   INTEGER AUTO_INCREMENT NOT NULL,
-    order_type      VARCHAR (35) NOT NULL,
+CREATE TABLE status_sales (
+    id_status_sale   INTEGER AUTO_INCREMENT NOT NULL,
+    status_sale      VARCHAR (35) NOT NULL,
 
-    PRIMARY KEY (id_order_type)
+    PRIMARY KEY (id_status_sale)
 );
-INSERT INTO orders_types (order_type) VALUES ('Disponible');
-INSERT INTO orders_types (order_type) VALUES ('Vendido y liquidado');
-INSERT INTO orders_types (order_type) VALUES ('Apartado');
-INSERT INTO orders_types (order_type) VALUES ('Vendido a plazos');
+INSERT INTO status_sales (status_sale) VALUES ('Disponible');
+INSERT INTO status_sales (status_sale) VALUES ('Vendido y liquidado');
+INSERT INTO status_sales (status_sale) VALUES ('Apartado');
+INSERT INTO status_sales (status_sale) VALUES ('Vendido a plazos');
 
 -- ORDERS--------------------------------------------------------------------------------------------------------------
 CREATE TABLE orders(
@@ -299,7 +299,6 @@ CREATE TABLE orders(
     date_at     DATETIME NOT NULL,
     amount      DOUBLE,
     coments     VARCHAR(250),
-
     PRIMARY KEY (id_order)
 );
 INSERT INTO orders (date_at, amount, coments)
@@ -310,38 +309,39 @@ INSERT INTO orders (date_at, amount, coments)
 VALUES ('2018-04-15 18:15:27', 526200, 'uno apartado y uno vendido');
 
 -- ORDERS DETAILS------------------------------------------------------------------------------------------------------
-CREATE TABLE orders_details (
-    id_order        INTEGER NOT NULL,
-    id_vehicle      INTEGER NOT NULL,
+CREATE TABLE sales (
+    id_sale         INTEGER NOT NULL,
+    id_car          INTEGER NOT NULL,
     id_customer     INTEGER NOT NULL,
     id_seller       INTEGER NOT NULL,
-    id_order_type   INTEGER NOT NULL,
+    id_status_sale  INTEGER NOT NULL,
+    id_order        INTEGER NOT NULL,
     deposit         DOUBLE NOT NULL,
 
-    KEY (id_order, id_vehicle, id_customer, id_seller, id_order_type),
+    KEY (id_sale, id_car, id_customer, id_seller, id_status_sale, id_order),
 
-    CONSTRAINT `orders_details_ibfk_1` FOREIGN KEY (id_order) REFERENCES orders (id_order),
+	CONSTRAINT `sales_ibfk_1` FOREIGN KEY (id_car) REFERENCES cars (id_car),
 
-	CONSTRAINT `orders_details_ibfk_2` FOREIGN KEY (id_vehicle) REFERENCES vehicles (id_vehicle),
+	CONSTRAINT `sales_ibfk_2` FOREIGN KEY (id_customer) REFERENCES customers (id_customer),
 
-	CONSTRAINT `orders_details_ibfk_3` FOREIGN KEY (id_customer) REFERENCES customers (id_customer),
+    CONSTRAINT `sales_ibfk_3` FOREIGN KEY (id_seller) REFERENCES sellers (id_seller),
 
-    CONSTRAINT `orders_details_ibfk_4` FOREIGN KEY (id_seller) REFERENCES sellers (id_seller),
+	CONSTRAINT `sales_ibfk_4` FOREIGN KEY (id_status_sale) REFERENCES status_sales (id_status_sale),
 
-	CONSTRAINT `orders_details_ibfk_5` FOREIGN KEY (id_order_type) REFERENCES orders_types (id_order_type)
+	CONSTRAINT `sales_ibfk_5` FOREIGN KEY (id_order) REFERENCES orders (id_order)
 );
-INSERT INTO orders_details (id_order, id_vehicle, id_customer, id_seller, id_order_type, deposit)
-VALUES (1, 2, 1, 1, 4, 75000);
-INSERT INTO orders_details (id_order, id_vehicle, id_customer, id_seller, id_order_type, deposit)
-VALUES (1, 3, 1, 1, 3, 75000);
-INSERT INTO orders_details (id_order, id_vehicle, id_customer, id_seller, id_order_type, deposit)
-VALUES (2, 7, 2, 2, 4, 75000);
-INSERT INTO orders_details (id_order, id_vehicle, id_customer, id_seller, id_order_type, deposit)
-VALUES (2, 8, 2, 2, 3, 75000);
-INSERT INTO orders_details (id_order, id_vehicle, id_customer, id_seller, id_order_type, deposit)
-VALUES (3, 11, 3, 2, 2, 254900);
-INSERT INTO orders_details (id_order, id_vehicle, id_customer, id_seller, id_order_type, deposit)
-VALUES (3, 12, 3, 2, 3, 75000);
+INSERT INTO sales (id_sale, id_car, id_customer, id_seller, id_status_sale, id_order, deposit)
+VALUES (1, 2, 1, 1, 4, 1, 75000);
+INSERT INTO sales (id_sale, id_car, id_customer, id_seller, id_status_sale, id_order, deposit)
+VALUES (1, 3, 1, 1, 3, 1, 75000);
+INSERT INTO sales (id_sale, id_car, id_customer, id_seller, id_status_sale, id_order, deposit)
+VALUES (2, 7, 2, 2, 4, 2, 75000);
+INSERT INTO sales (id_sale, id_car, id_customer, id_seller, id_status_sale, id_order, deposit)
+VALUES (2, 8, 2, 2, 3, 2, 75000);
+INSERT INTO sales (id_sale, id_car, id_customer, id_seller, id_status_sale, id_order, deposit)
+VALUES (3, 11, 3, 2, 2, 3, 254900);
+INSERT INTO sales (id_sale, id_car, id_customer, id_seller, id_status_sale, id_order, deposit)
+VALUES (3, 12, 3, 2, 3, 3, 75000);
 
 -- PAYMENTS-----------------------------------------------------------------------------------------
 CREATE TABLE payments (
