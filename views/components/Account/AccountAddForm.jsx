3866dd83f0d39ad115ac64472285d1f0eb2d6b50
Paco Preciado Mendoza <__pm@outlook.com>
2018-06-18 22:01:19 -0500

    :bulb: Add other account form to the global manager

--- views/components/Account/AccountAddForm.jsx
+++ views/components/Account/AccountAddForm.jsx
@@ -1,5 +1,4 @@
 const React = require('react');
-const PropTypes = require('prop-types');
 const TransitionGroup = require('react-addons-transition-group');
 
 // Modules
@@ -17,8 +16,16 @@ class AccountAddForm extends React.Component {
             visible       : props.visible,
             onPostData    : props.onPostData,
             options       : props.options,
-            messageFlash  : ''
+            filterOptions : '',
+            messageFlash  : '',
+            typeAccount   : props.typeAccount
             }
+
+        this.handleOnChange  = this.handleOnChange.bind(this);
+
+        console.log('CONSTRUJCTOR: ', this.state.options);
+        console.log('CONSTRUJCTOR props: ', props);
+
     }
 
      static getDerivedStateFromProps(nextProps, prevState) {
@@ -29,6 +36,8 @@ class AccountAddForm extends React.Component {
             }
         }
         if (nextProps.options !==  prevState.options){
+            console.log('NEXT OPTIONS: ',nextProps.options);
+
             return{
                 options  : nextProps.options
             }
@@ -37,74 +46,145 @@ class AccountAddForm extends React.Component {
             return null;
     }
 
-    // componentDidMount() {
-    //     let ajax = new XMLHttpRequest();
+    handleOnChange(e){
+        e.preventDefault();
+        let type = e.target.selectedOptions[0].value;
 
-    //     ajax.onload = () => {
-    //         if(ajax.status == 200) {
-    //             this.setState({
-    //                 options : ajax.responseText
-    //             })
-    //         }
-    //     }
-    //     ajax.open('GET', '/seller/SellersWithoutAccount', true); 
-    //     ajax.send(); 
-    // }
+        if( type == 'Sellers' )
+            this.setState( { filterOptions : this.state.options.sellers } );
+        else if ( type == 'Office Managers')
+            this.setState( { filterOptions : this.state.options.officeManagers } );
+        else
+            this.setState( { filterOptions : this.state.options.globalManagers } );
+    }
+
 
     render() {
+
+        if (this.state.typeAccount == 'globalManager')
         return (
-            <div id="addAccount" className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-lg">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Add new Account</h5>
-                            <button id='closeAddForm' type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                <div id="addAccount" className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Add new Account</h5>
+                                <button id='closeAddForm' type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <TransitionGroup>
+                                {this.state.visible ? <MessageFlash messageFlash = { this.state.messageFlash }
+                                    clase="alert alert-danger messageFlash"
+                                /> : null}
+                            </TransitionGroup>
+
+                            <form id='accountAddForm' className='my-3 p-3 bg-white rounded box-shadow' action='/account/addAccount' method='POST'>
+                                <Input
+                                    type='text'
+                                    name='username'
+                                    text='Username'
+                                />
+                                <Input
+                                    type='password'
+                                    name='password'
+                                    text='Password'
+                                />
+                                <Input
+                                    type='email'
+                                    name='email'
+                                    text='Email'
+                                />
+
+                                <Select
+                                    options = { 
+                                        [ 
+                                            { id : 1, name : 'Sellers', typeAccount : 'seller' },
+                                            { id : 2, name : 'Office Managers', typeAccount : 'officeManager' }, 
+                                            { id : 3, name : 'Global Managers', typeAccount : 'globalManager' } 
+                                        ] 
+                                    }
+                                    text    = { 'You have someone without an Account! Select a type user to see...' }
+                                    id      = 'select-account-add-form'
+                                    defaultOption = 'Select a type to filter who does not have an account'
+                                    someClick = { this.handleOnChange }
+                                />
+
+                                <Select
+                                    options = { this.state.filterOptions }
+                                    defaultOption = 'Select a someone to add an account'
+                                    id      = 'select-account-add-form'
+                                />
+
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <Buttom
+                                    type='reset'
+                                    name='buttom-save-account'
+                                    text='Save Changes'
+                                    someClick = { this.state.onPostData }
+                                />
+                                </div>                         
+                            </form>
                         </div>
-
-                        <TransitionGroup>
-                            {this.state.visible ? <MessageFlash messageFlash = { this.state.messageFlash }
-                                clase="alert alert-danger messageFlash"
-                            /> : null}
-                        </TransitionGroup>
-
-                        <form id='accountAddForm' className='my-3 p-3 bg-white rounded box-shadow' action='/account/addAccount' method='POST'>
-                            <Input
-                                type='text'
-                                name='username'
-                                text='Username'
-                            />
-                            <Input
-                                type='password'
-                                name='password'
-                                text='Password'
-                            />
-                            <Input
-                                type='email'
-                                name='email'
-                                text='Email'
-                            />
-                            <Select
-                                options = { this.state.options }
-                                text    = { 'You have sellers without an Account! Select a seller to add one...' }
-                                id      = 'select-account-add-form'
-                            />
-
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <Buttom
-                                type='reset'
-                                name='buttom-save-account'
-                                text='Save Changes'
-                                someClick = { this.state.onPostData }
-                            />
-                            </div>                         
-                        </form>
                     </div>
                 </div>
-            </div>
-        )
+            )
+        else
+            return (
+                <div id="addAccount" className="modal fade bd-example-modal-lg" tabIndex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Add new Account</h5>
+                                <button id='closeAddForm' type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+
+                            <TransitionGroup>
+                                {this.state.visible ? <MessageFlash messageFlash = { this.state.messageFlash }
+                                    clase="alert alert-danger messageFlash"
+                                /> : null}
+                            </TransitionGroup>
+
+                            <form id='accountAddForm' className='my-3 p-3 bg-white rounded box-shadow' action='/account/addAccount' method='POST'>
+                                <Input
+                                    type='text'
+                                    name='username'
+                                    text='Username'
+                                />
+                                <Input
+                                    type='password'
+                                    name='password'
+                                    text='Password'
+                                />
+                                <Input
+                                    type='email'
+                                    name='email'
+                                    text='Email'
+                                />
+                                <Select
+                                    options = { this.state.options }
+                                    defaultOption = 'Select a someone to add an account'
+                                    text    = { 'You have sellers without an Account! Select a seller to add one...' }
+                                    id      = 'select-account-add-form'
+                                />
+
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <Buttom
+                                    type='reset'
+                                    name='buttom-save-account'
+                                    text='Save Changes'
+                                    someClick = { this.state.onPostData }
+                                />
+                                </div>                         
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )
     }
 }
 
