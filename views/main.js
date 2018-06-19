c572ec67608b322f541fe6fc78e975dfc3f54361
Paco Preciado Mendoza <__pm@outlook.com>
2018-06-18 22:06:05 -0500

    :sparkles: Require All the principal views

--- views/main.js
+++ views/main.js
@@ -1,39 +1,52 @@
 // Dependencies
-const React     = require('react');
-const ReactDOM  = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 require('babel-polyfill');
-// Module
-const Login         = require('./components/Login/Login.jsx');
-const OfficeManager = require('./OfficeManager.jsx');
-const Test          = require('./Test.jsx');
-const Err           = require('./Error.jsx');
 import registerServiceWorker from './registerServiceWorker';
 
-let main = function(data, view,  containerId) {
-  const container = document.getElementById(containerId || 'content-body');
-  
+// Module
+const Login 		= require('./components/Login/Login.jsx');
+const Seller 		= require('./Seller.jsx');
+const OfficeManager = require('./OfficeManager.jsx');
+const GlobalManager = require('./GlobalManager.jsx');
+const Test 			= require('./Test.jsx');
+const Err 			= require('./Error.jsx');
 
-   switch(view)
-  {
-    case 'Login':
-      console.log('Case Login')
-      ReactDOM.render(<Login {...data} />, container);
+let main = function (data, view, containerId) {
+	const container = document.getElementById(containerId || 'content-body');
 
-    break
-    case 'OfficeManager':
-    console.log('Case OfficeManager')
 
-      ReactDOM.render(<OfficeManager {...data} />, container);
-    break
-    case 'Test':
-    console.log('Case Test')
-      ReactDOM.render(<Test {...data} />, container);
-    break
-    case 'Error':
-    console.log('Case Error')
-      ReactDOM.render(<Err {...data} />, container);
-    break
-  }
+	switch (view) {
+		case 'Login':
+			console.log('Case Login')
+			ReactDOM.render(<Login {...data} />, container);
+			break
+
+		case 'Seller':
+			console.log('Case Seller')
+			ReactDOM.render(<Seller {...data} />, container);
+			break
+
+		case 'OfficeManager':
+			console.log('Case OfficeManager')
+			ReactDOM.render(<OfficeManager {...data} />, container);
+			break
+
+		case 'GlobalManager':
+			console.log('Case GlobalManager')
+			ReactDOM.render(<GlobalManager {...data} />, container);
+			break
+			
+		case 'Test':
+			console.log('Case Test')
+			ReactDOM.render(<Test {...data} />, container);
+			break
+
+		case 'Error':
+			console.log('Case Error')
+			ReactDOM.render(<Err {...data} />, container);
+			break
+	}
 };
 registerServiceWorker();
 
